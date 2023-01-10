4c6d79fcc91ddac30d3bdf28993f7dd043e23b48
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-10 11:01:12 -0600

    break big file apart into separate modules

---
+++ another_sudoku_library/generate.py
@@ -0,0 +1,97 @@
+import random
+from .utils import fullGen, exclusiveGen, getEmptyBoard, copyBoard
+from .solve import generateCache, uniqueCheck, checkComplete, checkConsistent, solve
+
+
+def picker(board, y, x, cache=None):
+    #short-circuit, if y/x has val already
+    if board[y][x] != 0:    
+        return board[y][x]
+    
+    # cache is optional arg, may need to calc
+    if cache == None:       
+        cache = generateCache(board)
+
+    #uniqueCheck returns value, or -1 for error
+    slv = uniqueCheck(board, y, x, cache=cache)
+    if slv != 0:      
+        return slv
+    
+    # if no existing val, solve, or error, pick randomly from valid opt    
+    else:             
+        options = list(cache[y][x])
+        return options[random.randint(0, len(options)-1)]
+
+def generate():
+    done = False
+    while not done:
+        n = getEmptyBoard()
+        for y, x in fullGen():          # loop through entire board
+            n[y][x] = picker(n, y, x)   # picker solves cell if possible, otherwise gives random
+
+            if y > 5:                   # heuristic: once board is mostly full
+                n = solve(n, nest=4)    # start trying to solve remaining cells
+                if checkComplete(n):    
+                    break
+
+        if checkConsistent(n):
+            done = True
+    return n
+
+def removable(board, y, x, nest=4):
+    """ Tests whether board can still be solved without given y/x cell. Returns true/false"""
+    test = copyBoard(board)        
+    test[y][x] = 0
+    test = solve(test, nest=nest)  # test to see if board is still solveable after remove
+
+    if checkComplete(test) and checkConsistent(test):
+        return True 
+    else:
+        return False
+
+def rm(wb, forbid, ry, rx, nest=4):
+    """checks whether a cell is removable or not. If yes, removes it. Returns 1 or 0, if removed.
+    
+    This is NOT PURE. It changes the wb and forbid lists that are passed to it.
+    """
+    if wb[ry][rx] == 0 or forbid[ry][rx] == 1: 
+        return 0
+    if removable(wb, ry, rx, nest=nest):       # if board solveable after removing this coord
+        wb[ry][rx] = 0              # remove that cell and continue
+        return 1
+    else:
+        forbid[ry][rx] = 1          # if test fails, forbid value so we dont retest it
+        return 0
+
+def carve(board, count=60):
+    """ Takes a full board, and removes cells so that puzzle is still solveable. Removes up to
+    count cells (if it can. In practice, we're removing ~60 cells max right now)
+    """
+    wb = copyBoard(board)
+    forbid = getEmptyBoard()            # if a cell is necessary for solve, save coords 
+    removes = 0
+    attempts = 0
+
+    # stage one: try removing random coords (up to 50 times
+    while removes < count and attempts < 50:  
+        attempts += 1
+        ry = random.randint(0, 8)  
+        rx = random.randint(0, 8)
+        if rm(wb, forbid, ry, rx):      # rm() tries remove, updates wb and forbid, returns t/f
+            removes += 1
+            
+    # stage two: crawl entire board and try remove
+    if removes < count:
+        for ry, rx in exclusiveGen():
+            if rm(wb, forbid, ry, rx): # nest=1):   # can use nesting to get ~5 extra carves (at time cost)
+                removes += 1
+                if removes == count:
+                    break
+                
+    #print(removes)
+    return wb 
+
+def getPuzzle(diff=40):
+    """feed it the number of cells to remove. 60 is hard, 25 is easy"""
+    return carve(generate(), count=diff)
+
---
+++ another_sudoku_library/solve.py
@@ -0,0 +1,184 @@
+from .utils import rowGen, colGen, sqrGen, fullGen, getEmptyBoard, copyBoard
+
+
+# Return list of KNOWN values within a row/sqr/col.         
+def getRowVals(board, y, x, inclusive=True):
+    """Returns list of values of already set cells in row"""
+    out = []
+    for y1, x1 in rowGen(y, x, inclusive=inclusive):
+        if board[y1][x1] == 0:
+            continue 
+        elif type(board[y][x]) == int:
+            out.append(board[y1][x1])
+        else:
+            out += board[y1][x1]
+    return out   
+    
+def getColVals(board, y, x, inclusive=True):
+    """Returns list of values of already set cells in col"""
+    out = []
+    for y1, x1 in colGen(y, x, inclusive=inclusive):
+        if board[y1][x1] == 0:
+            continue 
+        elif type(board[y][x]) == int:
+            out.append(board[y1][x1])
+        else:
+            out += board[y1][x1]
+    return out
+
+def getSqrVals(board, y, x, inclusive=True):
+    """Returns list of values of already set cells in sqr"""
+    out = []
+    for y1, x1 in sqrGen(y, x, inclusive=inclusive):
+        if board[y1][x1] == 0:
+            continue 
+        elif type(board[y][x]) == int:
+            out.append(board[y1][x1])
+        else:
+            out += board[y1][x1]
+    return out
+
+def getPoss(board, y, x):
+    """Returns set of possible values for a cell. Returns single-value set if cell is decided."""
+    if board[y][x] != 0:
+        return [board[y][x]]
+
+    return ({1,2,3,4,5,6,7,8,9}
+            -set(getRowVals(board, y, x))
+            -set(getColVals(board, y, x))
+            -set(getSqrVals(board, y, x)))
+
+def countZeros(board):
+    """Returns count of zeros (eg carved cells) in a provided board. 
+    
+    Should work on stringify() board, and also full list board"""
+    
+    if type(board) == str:
+        return board.count('0')
+        
+    zeros = 0
+    for y, x in fullGen():
+        if board[y][x] == 0:
+            zeros += 1
+    return zeros
+
+
+
+def generateCache(board):
+    """returns parallel cache board, holding set of possibilities for each cell"""
+    out = getEmptyBoard()
+    for y, x in fullGen():
+        out[y][x] = getPoss(board, y, x)
+    return out
+    
+def uniqueCheck(board, y, x, cache=None):
+    """Tries to solve cell. Return 1-9 if found, 0 if inconclusive, -1 if no possibilities. 
+    
+        Looks like this is 16x faster w cache provided. Also if we calc cache here, we're doing a lot of
+        work to get possibilities for cells we won't access. 
+        
+        Works by getting the set of possible values
+        for the cell, then comparing with the possible values for every other cell in its row/col/sqr.
+    """
+    
+    #run cheap, naive checks first
+    cellPoss = getPoss(board, y, x)
+    if len(cellPoss) == 1:      #if cell has one poss, must be answer
+        return cellPoss.pop()
+    elif len(cellPoss) == 0:    #if no poss for cell, must be error in board
+        return -1
+
+    #if no cache, we do have to generate one
+    if cache == None:
+        cache = generateCache(board)
+
+    #compare cell possibilities to unit possibilities. Looking for UNIQUE poss in cell
+    if cellPoss - set(getRowVals(cache, y, x, inclusive=False)):
+        return (cellPoss - set(getRowVals(cache, y, x, inclusive=False))).pop()
+    if cellPoss - set(getColVals(cache, y, x, inclusive=False)):
+        return (cellPoss - set(getColVals(cache, y, x, inclusive=False))).pop()
+    if cellPoss - set(getSqrVals(cache, y, x, inclusive=False)):
+        return (cellPoss - set(getSqrVals(cache, y, x, inclusive=False))).pop()
+    
+    #if inconclusive, just return nothing
+    return 0
+            
+def solve(board, nest=0):
+    """This is best-effort solve. Tries its best, but may return an incomplete or inconsistent 
+        board. If error, should have that cell as -1. 
+    """
+    wb = copyBoard(board)   #pure function--dont change original board arg
+    c = generateCache(wb)
+    solved = False
+    changed = True
+
+    while not solved and changed:           # iterate over board
+        changed = False
+        for y, x in fullGen():              # use uniqueCheck() against whole board
+            if wb[y][x] == 0:               # but skip already solved cells
+                wb[y][x] = uniqueCheck(wb, y, x, cache=c)
+                if wb[y][x] > 0:
+                    c = generateCache(wb)       #regen cache if we hit an answer
+                    changed = True              #if solved, raise the flag
+
+        if checkComplete(wb):               #now check if it's finished
+            solved = True
+
+    #recurse clause (up to 3 times)
+    if not solved and nest < 3:
+        lpy, lpx = 0, 0     #lynchpin cell coords. Find cell with most possibilities.
+        for y, x in fullGen():
+            if len(c[y][x]) > len(c[lpy][lpx]):
+                lpy, lpx = y, x
+
+        #now we check EACH possibility for lynchpin, to see if we get a solve
+        for value in c[lpy][lpx]:
+            testWB = copyBoard(wb)              #testWB is a branch--doesn't change orig state of wb
+            testWB[lpy][lpx] = value            #make assumption about lynchpin  
+            testWB = solve(testWB, nest=nest+1) #try to solve
+
+            if checkComplete(testWB) and checkConsistent(testWB):
+                if not solved:
+                    wb = testWB
+                    solved = True
+                else:                           #if board already solved, means multiple solutions
+                    print("Error: recursion uncovered multiple valid solutions")
+                    wb[0][0] = 0                #in case of multiple valid solutions, fail the board
+
+    #output the now-solved (maybe incomplete) (and maybe inconsistent) board
+    return wb 
+
+def checkComplete(board):
+    """Naive check whether each cell has been filled (or if its still 0)"""
+    for y, x in fullGen():
+        if board[y][x] == 0:
+            return False 
+    return True
+        
+def checkConsistent(board):
+    """Loop through board. If a cell matches another cell in its unit, its not consistent"""
+
+    #check for -1 first, this automatically means error
+    if not checkConsistentCheap(board):
+        return False
+    
+    for y, x in fullGen():
+        if board[y][x] == 0:    #ignore unset cells
+            continue  
+
+        temp = (  
+            getRowVals(board, y, x, inclusive=False) 
+            + getColVals(board, y, x, inclusive=False)
+            + getSqrVals(board, y, x, inclusive=False)
+        )
+        if board[y][x] in temp:
+            return False
+
+    return True
+
+def checkConsistentCheap(board):
+    for y, x in fullGen():
+        if board[y][x] == -1:
+            return False
+    return True
+
