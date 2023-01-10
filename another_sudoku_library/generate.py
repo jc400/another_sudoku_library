fa98064e5e635ef71457887c9739c794ea828df5
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-10 14:56:43 -0600

    update docstrings for generate

--- another_sudoku_library/generate.py
+++ another_sudoku_library/generate.py
@@ -6,36 +6,40 @@ from .solve import _generateCache, _uniqueCheck, checkComplete, checkConsistent,
 
 
 def _picker(board, y, x, cache=None):
-    # short-circuit, if y/x has val already
+    """Returns value for cell on board. First checks for existing value, 
+    
+    then checks if there's only one possible value, finally chooses randomly
+    among valid possibilities.
+    """
     if board[y][x] != 0:
         return board[y][x]
 
-    # cache is optional arg, may need to calc
     if cache == None:
         cache = _generateCache(board)
 
-    # uniqueCheck returns value, or -1 for error
-    slv = _uniqueCheck(board, y, x, cache=cache)
-    if slv != 0:
-        return slv
+    solve = _uniqueCheck(board, y, x, cache=cache)
+    if solve != 0:
+        return solve
 
-    # if no existing val, solve, or error, pick randomly from valid opt
     else:
         options = list(cache[y][x])
         return options[random.randint(0, len(options) - 1)]
 
 
 def _generate():
+    """Randomly generates a completely solved, logically consistent Sudoku board. 
+    
+    This doesn't work every time, so 'while not done:' loop continues trying until a 
+    consistent board is generated.
+    """
     done = False
     while not done:
         n = getEmptyBoard()
-        for y, x in fullGen():  # loop through entire board
-            n[y][x] = _picker(
-                n, y, x
-            )  # picker solves cell if possible, otherwise gives random
+        for y, x in fullGen():
+            n[y][x] = _picker(n, y, x)
 
-            if y > 5:  # heuristic: once board is mostly full
-                n = solve(n, nest=4)  # start trying to solve remaining cells
+            if y > 5:  # heuristic: try to solve once board is mostly full
+                n = solve(n, nest=4)
                 if checkComplete(n):
                     break
 
@@ -45,69 +49,64 @@ def _generate():
 
 
 def _removable(board, y, x, nest=4):
-    """Tests whether board can still be solved without given y/x cell. Returns true/false"""
+    """Tests if board can be solved without given cell. Returns bool. """
     test = copyBoard(board)
     test[y][x] = 0
-    test = solve(
-        test, nest=nest
-    )  # test to see if board is still solveable after remove
-
-    if checkComplete(test) and checkConsistent(test):
-        return True
-    else:
-        return False
+    test = solve(test, nest=nest)  
+    return checkComplete(test) and checkConsistent(test)
 
 
 def _rm(wb, forbid, ry, rx, nest=4):
-    """checks whether a cell is removable or not. If yes, removes it. Returns 1 or 0, if removed.
-
-    This is NOT PURE. It changes the wb and forbid lists that are passed to it.
+    """Checks if cell is removable or not. If yes, removes it from wb. If not, adds 
+    
+    that cell to the forbid list. Return True if removed, False if not. Nest arg controls
+    how hard it tries to solve puzzle without the given cell--higher nest is slower, but
+    removes more cells.
     """
-    if wb[ry][rx] == 0 or forbid[ry][rx] == 1:
+    if wb[ry][rx] == 0 or forbid[ry][rx] == 1: # skip
         return 0
-    if _removable(
-        wb, ry, rx, nest=nest
-    ):  # if board solveable after removing this coord
-        wb[ry][rx] = 0  # remove that cell and continue
-        return 1
+
+    if _removable(wb, ry, rx, nest=nest): 
+        wb[ry][rx] = 0
+        return True
     else:
-        forbid[ry][rx] = 1  # if test fails, forbid value so we dont retest it
-        return 0
+        forbid[ry][rx] = 1
+        return False
 
 
 def _carve(board, count=60):
-    """Takes a full board, and removes cells so that puzzle is still solveable. Removes up to
-    count cells (if it can. In practice, we're removing ~60 cells max right now)
+    """Takes a full board, and removes cells so that puzzle is still solveable. 
+    
+    Removes up to count cells. Note that there is a limit, you typically can't remove
+    more than ~60 cells without making puzzle unsolveable. More cells removed = harder
+    puzzle.
     """
     wb = copyBoard(board)
-    forbid = getEmptyBoard()  # if a cell is necessary for solve, save coords
+    forbid = getEmptyBoard()  # cache cells that are necessary to solve puzzle
     removes = 0
     attempts = 0
 
-    # stage one: try removing random coords (up to 50 times
+    # stage one: try removing random coords
     while removes < count and attempts < 50:
         attempts += 1
         ry = random.randint(0, 8)
         rx = random.randint(0, 8)
-        if _rm(
-            wb, forbid, ry, rx
-        ):  # rm() tries remove, updates wb and forbid, returns t/f
+        if _rm(wb, forbid, ry, rx): 
             removes += 1
 
     # stage two: crawl entire board and try remove
     if removes < count:
         for ry, rx in exclusiveGen():
-            if _rm(
-                wb, forbid, ry, rx
-            ):  # nest=1):   # can use nesting to get ~5 extra carves (at time cost)
+            if _rm(wb, forbid, ry, rx):
                 removes += 1
                 if removes == count:
                     break
 
-    # print(removes)
     return wb
 
 
 def getPuzzle(diff=40):
-    """feed it the number of cells to remove. 60 is hard, 25 is easy"""
+    """Generates completed puzzle, then carves off cells to create puzzle. 
+    
+    Diff argumnet is the number of cells to remove. 60 is hard, 25 is easy"""
     return _carve(_generate(), count=diff)
