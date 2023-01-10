631eb08eb8a4e92fecf843b264911ece9029a559
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-10 12:22:52 -0600

    add test for solve()

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,14 +1,26 @@
-from another_sudoku_library.solve import _getRowVals, _getColVals, _getSqrVals
-from .sample_boards import t5
+import pytest
+from another_sudoku_library.solve import _getRowVals, _getColVals, _getSqrVals, solve
+from . import sample_boards
 
 
 def test_getRowVals():
-    assert _getRowVals(t5, 3, 7, inclusive=False) == [6, 5, 7]
+    assert _getRowVals(sample_boards.t1, 3, 7, inclusive=False) == [6, 5, 7]
 
 
 def test_getColVals():
-    assert _getColVals(t5, 1, 1, inclusive=False) == [2, 4, 5]
+    assert _getColVals(sample_boards.t1, 1, 1, inclusive=False) == [2, 4, 5]
 
 
 def test_getSqrVals():
-    assert _getSqrVals(t5, 4, 4, inclusive=False) == [5, 7]
+    assert _getSqrVals(sample_boards.t1, 4, 4, inclusive=False) == [5, 7]
+
+
+@pytest.mark.parametrize(
+    "pz",
+    (
+        {"problem": sample_boards.t2_problem, "solution": sample_boards.t2_solution},
+        {"problem": sample_boards.t3_problem, "solution": sample_boards.t3_solution},
+    ),
+)
+def test_solve_t2(pz):
+    assert solve(pz["problem"]) == pz["solution"]
