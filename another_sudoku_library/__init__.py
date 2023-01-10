47a3537376d22357f51330684b3290d46182a015
jc400 <105320127+jc400@users.noreply.github.com>
2023-01-10 11:10:55 -0600

    expose public methods in top-level package

--- another_sudoku_library/__init__.py
+++ another_sudoku_library/__init__.py
@@ -3,3 +3,7 @@
 __author__ = """jc400"""
 __email__ = ' '
 __version__ = '0.1.0'
+
+from .utils import getEmptyBoard, copyBoard, getBitmap, fullGen, shuffle
+from .solve import checkComplete, checkConsistent, check
+from .generate import getPuzzle
